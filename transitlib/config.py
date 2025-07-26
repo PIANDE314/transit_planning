a2188d22c7aa1b865fc6d142e4183486ed6a02ae
PIANDE314 <147113909+PIANDE314@users.noreply.github.com>
2025-07-26 14:50:56 -0400

    Update config.py

--- transitlib/config.py
+++ transitlib/config.py
@@ -42,8 +42,11 @@ DEFAULTS: Dict[str, Any] = {
     "K_neg":             5,       # max new negative seeds per iter
     "pos_thresh":        0.95,    # probability threshold for positives
     "neg_thresh":        0.80,    # probability threshold for negatives
+    "logreg_neg_tresh":  0.90,    # treshold for label-0 segments
+    "noise_thresh_frac": 0.5,     # trigger for noise injection
     "self_max_iters":    200,     # max self‑training iterations
     "self_test_size":    0.2,     # validation split for initial model
+    "self_runs":         100,     # number of self-training runs
     "random_state":      0,       # global RNG seed
 
     # === Stop extraction (§ 3.2) ===
