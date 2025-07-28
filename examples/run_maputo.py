3ecb7106eb7c8c564d48f9ca368f4d3abef34312
PIANDE314 <147113909+PIANDE314@users.noreply.github.com>
2025-07-28 17:51:40 -0400

    Update run_maputo.py

--- examples/run_maputo.py
+++ examples/run_maputo.py
@@ -6,7 +6,7 @@ import pandas as pd
 import osmnx as ox
 from transitlib.config import Config
 from transitlib.data.download import (
-    download_file, fetch_worldpop_url, extract_worldpop_tif,
+    download_file, fetch_worldpop_cog_crop, extract_worldpop_tif,
     fetch_hdx_rwi_url
 )
 from transitlib.data.osm import load_osm_network, load_osm_pois
